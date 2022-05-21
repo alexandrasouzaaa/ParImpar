ea76531944ad683fee8ef04fcd7fb9f66a583c47
alexandra <alexandradeh643@gmail.com>
2022-05-20 22:37:19 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿int numero;
+
+Console.WriteLine();
+
+Console.Write("Digite um número: ");
+numero = Convert.ToInt32(Console.ReadLine());
+
+if (numero % 2 == 0)
+{ 
+    Console.WriteLine($"O número {numero} é par!");
+}
+else
+{
+        Console.WriteLine($"O número {numero} é ímpar!");
+}
+
+Console.WriteLine();
